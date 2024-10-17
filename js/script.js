21b75534425d12ffaf37e2a9be3a68d7bdfa9cd9
MadMath02 <diegopiccoli2002@gmail.com>
2024-10-17 14:56:02 +0200

    Fatto script di autoaggiunta del contenuto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-var loadedText;
-
 function makePage(){
-	
+	let domande=getDomande();
+	let body=document.getElementById("body");
+	let i;
+	for(i=0;i<domande.length;i++){
+		let toAdd="<div class=\"intervistatore\">\n <div class=\"intervistatore interno\">\n<p id=\"domanda"+i+"\" class=\"intervistatore\">"+domande[i].Q+"</p>\n</div>\n</div>\n";
+		toAdd+= "<div class=\"intervistato\">\n <div class=\"intervistato interno\">\n<p id=\"risposta"+i+"\" class=\"intervistato\">"+domande[i].A+"</p>\n</div>\n</div>\n";
+		let text=body.innerHTML;
+		body.innerHTML=text+toAdd;
+	}
+	for(i=0;i<domande.length;i++){
+		document.getElementById("domanda"+i).style="max-width:"+(document.getElementById("domanda"+i).innerHTML.length*0.85)+"ch;";
+		document.getElementById("risposta"+i).style="max-width:"+(document.getElementById("risposta"+i).innerHTML.length*0.85)+"ch;";
+	}
+}
